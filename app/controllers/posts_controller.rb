1b961b55f4adbf1d555dc3b818eb873b957eec8d
Devin Wieland <devinwieland@gmail.com>
2014-03-31 21:20:50 -0500

    switched over to rails form helper for creating posts...

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,16 +4,20 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
     @categories = Category.all
     render "posts/create"
   end
 
   def create
-    @post = Post.create!(title: params[:title],
-                         price: params[:price],
-                         description: params[:description],
-                         email: params[:email],
-                         category_id: params[:category])
-    redirect_to show_post_path(@post)
+    puts params
+    @post = Post.create!(post_params)
+    redirect_to show_post_path(@post.id)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:category_id, :title, :description, :email, :price)
   end
 end
